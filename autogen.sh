aaedeb3ae3d40688c7bcbda6fbced8d0c379ea02
Nick Schermer <nick@xfce.org>
2009-10-19 13:53:32 +0200

    Use new xdt build stuff.

--- autogen.sh
+++ autogen.sh
@@ -18,36 +18,6 @@ EOF
   exit 1
 }
 
-# verify that po/LINGUAS is present
-(test -f po/LINGUAS) >/dev/null 2>&1 || {
-  cat >&2 <<EOF
-autogen.sh: The file po/LINGUAS could not be found. Please check your snapshot
-            or try to checkout again.
-EOF
-  exit 1
-}
-
-# substitute revision and linguas
-linguas=`sed -e '/^#/d' po/LINGUAS`
-if test -d .git/svn; then
- revision=`git svn find-rev origin/trunk ||
-           git svn find-rev trunk ||
-           git svn find-rev HEAD ||
-           git svn find-rev master`
-else
- revision=`LC_ALL=C svn info $0 | awk '/^Revision: / {printf "%05d\n",
-$2}'`
-fi
-sed -e "s/@LINGUAS@/${linguas}/g" \
-    -e "s/@REVISION@/${revision}/g" \
-    < "configure.in.in" > "configure.in"
-
-xdt-autogen $@
-
-# xdt-autogen clean does not remove all generated files
-(test x"clean" = x"$1") && {
-  rm -f configure.ac
-  rm -f INSTALL
-} || true
+XDT_AUTOGEN_REQUIRED_VERSION="4.7.2" exec xdt-autogen $@
 
 # vi:set ts=2 sw=2 et ai:
