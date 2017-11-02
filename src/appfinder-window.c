b3dd6e547011793a3237c202e04d577d95943780
Andre Miranda <andreldm@xfce.org>
2017-11-02 17:35:11 -0300

    Use gtk_menu_popup to support gtk 3.20

--- src/appfinder-window.c
+++ src/appfinder-window.c
@@ -1093,7 +1093,14 @@ xfce_appfinder_window_popup_menu (GtkWidget           *view,
           G_CALLBACK (xfce_appfinder_window_popup_menu_hide), window);
       gtk_widget_show (mi);
 
+#if GTK_CHECK_VERSION (3, 22, 0)
       gtk_menu_popup_at_pointer (GTK_MENU (menu), NULL);
+#else
+      gtk_menu_popup (GTK_MENU (menu),
+                      NULL, NULL, NULL, NULL, 3,
+                      gtk_get_current_event_time ());
+#endif
+
       return TRUE;
     }
 
