f9062845cdd01b56fb329f8349a9feca61d6a4fc
Alexander Schwinn <alexxcons@xfce.org>
2024-10-23 15:41:56 +0200

    Update copyright year

--- src/main.c
+++ src/main.c
@@ -256,7 +256,7 @@ main (gint argc, gchar **argv)
   if (opt_version)
     {
       g_print ("%s %s (Xfce %s)\n\n", PACKAGE_NAME, PACKAGE_VERSION, xfce_version_string ());
-      g_print ("%s\n", "Copyright (c) 2004-2022");
+      g_print ("%s\n", "Copyright (c) 2004-2024");
       g_print ("\t%s\n\n", _("The Xfce development team. All rights reserved."));
       g_print (_("Please report bugs to <%s>."), PACKAGE_BUGREPORT);
       g_print ("\n");
