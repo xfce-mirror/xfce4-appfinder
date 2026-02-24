8be93cd144de5409e997dfcb70e070acd285e40f
Andre Miranda <andreldm@xfce.org>
2026-02-25 00:05:35 +0100

    Avoid crash freeing invalid pointer.

--- src/appfinder-actions.c
+++ src/appfinder-actions.c
@@ -367,7 +367,7 @@ xfce_appfinder_actions_load (XfceAppfinderActions *actions,
           if (action->name == NULL)
             {
               gchar *default_name = xfce_appfinder_actions_get_default_name (action->type, action->pattern);
-              action->name = default_name != NULL ? g_strdup (default_name) : "";
+              action->name = g_strdup (default_name != NULL ? default_name : "");
               g_free (default_name);
             }
         }
